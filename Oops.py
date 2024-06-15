5e6298709a55ba2dddb042d8c0d017eb4486a373
maharajafz03 <maharajafz1999@gmail.com>
2024-06-15 22:10:19 +0530

    class & objects

---
+++ Oops.py
@@ -0,0 +1,50 @@
+
+
+# a = 5
+# b = 5 + 10j
+
+
+# my_list = [1,2,3,4,5,6]
+
+# my_list[0] = 10
+# print(my_list)
+# print(type(b))
+
+str = ["magaraja", "yamuna", "keeru"]
+
+name = str[0]
+first_name = name[0:4]
+
+female = str[1]
+lastname = female[0:4]
+
+print(first_name + " loves " + lastname)
+
+names = ["magaraja", "yamuna", "keeru"]
+
+first_name = names[0][:4]
+last_name = names[1][:4]
+
+print(f'{first_name} loves {last_name}')
+
+a = '5'
+b = '5'
+
+print(f'{a} + {b}')
+
+name = ['raja', 'yamunna', 'keeru']
+
+name.append('maga')
+name.insert(-2, "fucker")
+result = name[0][0:3]
+
+print(result)
+print(name)
+
+url ={"name","age","firstname"}
+
+url.add("name", 3)
+
+print(url)
+
+
