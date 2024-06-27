26cea3c888ceb463566a75dadd0894036aad2b24
maharajafz03 <maharajafz1999@gmail.com>
2024-06-27 18:50:47 +0530

    constructor

---
+++ view/constructor.py
@@ -0,0 +1,14 @@
+class Name:
+
+    def __init__(self, name , age):
+        self.Name = name
+        self.age = age
+
+person = Name("magga", 29)
+
+
+        
+print(person.Name)
+print(person.age)
+
+
