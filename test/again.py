e2565ba3d96b881f92e0d3d5bf823e1b133e9971
maharajafz03 <maharajafz1999@gmail.com>
2024-08-08 08:25:02 +0530

    [added]

--- test/again.py
+++ test/again.py
@@ -23,4 +23,32 @@ class Link:
       while (cur is not None):
         cur.Pointer = newnode
 
+  def print(self,data):
+    print(self.data)
     
+
+  def remove(self,data):
+    if(self.head.Pointer is not None):
+         if(self.head.data == data):
+            self.head = self.head.Pointer 
+         else:
+           cur = self.head
+           while(cur.Pointer is not None and cur.Pointer.data != data):
+             cur = cur.Pointer
+
+             if cur.pointer is not None:
+               cur.pointer =  cur.pointer.pointer
+    
+    else:
+      print("linkedlist is empty")
+
+Linked = Link()
+
+Linked.add("node1")
+Linked.add("node2")
+Linked.add("node3")
+Linked.add("node4")
+
+Linked.remove("node3")
+
+print(Linked.data)
